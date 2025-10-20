766ab191020684ce3717ac0a35abce07b1da5fb1
Michael Lechner <mlechner@onzack.com>
2025-10-20 16:57:51 +0200

    chore: update provider documentation and group name from template to cloudscale in v1alpha1 API

--- apis/cluster/v1alpha1/doc.go
+++ apis/cluster/v1alpha1/doc.go
@@ -1,5 +1,5 @@
-// Package v1alpha1 contains the core resources of the template jet provider.
+// Package v1alpha1 contains the core resources of the cloudscale provider.
 // +kubebuilder:object:generate=true
-// +groupName=template.crossplane.io
+// +groupName=cloudscale.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
