9b2a00c8b1e93d6f353becb362aeaea7bced5bf7
Michael Lechner <mlechner@onzack.com>
2025-11-19 15:57:52 +0100

    chore: remove provider file and update volume configuration for server reference consistency

--- config/namespaced/volume/config.go
+++ config/namespaced/volume/config.go
@@ -9,20 +9,20 @@ func Configure(p *config.Provider) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "cloudscale"
 
-	// This resource need the server in which volume would be created
-	// as an input. And by defining it as a reference to github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server
-	// object, we can build cross resource referencing. See
-	// serverRef in the example in the Testing section below.
-	r.References["server"] = config.Reference{
-		Type: "github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server",
-	}
+		// This resource need the server in which volume would be created
+		// as an input. And by defining it as a reference to github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server
+		// object, we can build cross resource referencing. See
+		// serverRef in the example in the Testing section below.
+		r.References["server"] = config.Reference{
+			Type: "github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server",
+		}
 
-	// This resource need the server UUIDs to which volume would be attached
-	// as an input. And by defining it as a reference to github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server
-	// object, we can build cross resource referencing. See
-	// serverUuidsRefs in the example in the Testing section below.
-	r.References["server_uuids"] = config.Reference{
-		Type: "github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server",
-	}
-})
+		// This resource need the server UUIDs to which volume would be attached
+		// as an input. And by defining it as a reference to github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server
+		// object, we can build cross resource referencing. See
+		// serverUuidsRefs in the example in the Testing section below.
+		r.References["server_uuids"] = config.Reference{
+			Type: "github.com/onzack/provider-cloudscale/apis/namespaced/cloudscale/v1alpha1.Server",
+		}
+	})
 }
